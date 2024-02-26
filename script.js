95b0b14982e45f8d9c0f02971fde25b5f8693918
JSDID <facebook3800@ukr.net>
2024-02-26 11:04:30 +0200

    Added var and body

---
+++ script.js
@@ -0,0 +1,20 @@
+// number
+const age = 30;
+
+// string
+const name = "Jane";
+
+// object
+const fullName = {first: 'Jane', last: 'Doe'};
+
+// boolean
+const truth = true || false;
+
+// array
+const sheets = ["HTML", "CSS", "SCSS", "Script"];
+
+// undefined
+var a; typeof a;
+
+// value
+var a = null;
